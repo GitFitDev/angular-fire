1115fec8de775044983671f3a14ee2bd9cee8502
GitFitDev <revadacourtney@gmail.com>
2020-04-28 06:16:03 -0700

    refactor: update new training component
    refacrtored new training company to access information from the db with AngularFire

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
+import { map } from 'rxjs/operators';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 
@@ -12,12 +12,24 @@ import { Exercise } from '../exercise.model';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
-  ngOnInit() {
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+  ngOnInit(): void {
+    this.exercises = this.db
+      .collection("availableExercises")
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((doc) => {
+            return {
+              id: doc.payload.doc.id,
+              ...(doc.payload.doc.data() as Exercise),
+            };
+          });
+        })
+      );
   }
 
   onStartTraining(form: NgForm) {
