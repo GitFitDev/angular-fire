a7c4a82237dcbed1e6648e944e69966522855d28
GitFitDev <revadacourtney@gmail.com>
2020-05-06 06:24:45 -0700

    feat: build for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDVnXT6LE83vFxkXbC70cApLrMPsRJiBII",
+    authDomain: "ng-fitness-tracker-3b467.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-3b467.firebaseio.com",
+    projectId: "ng-fitness-tracker-3b467",
+    storageBucket: "ng-fitness-tracker-3b467.appspot.com",
+    messagingSenderId: "832660501069",
+    appId: "1:832660501069:web:c2a04d8e4df87b0a132204",
+    measurementId: "G-SFF707KEFR"
+    }
 };
